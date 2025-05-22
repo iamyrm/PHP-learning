a7d558c410e0951db5658d6e1b720a76b0f01e60
Yagyaraj Majhi <majhiyagya123@gmail.com>
2025-05-22 20:50:32 +0545

    Associative Arrays

---
+++ 2. associative arrays/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays in PHP</title>
+</head>
+
+<body>
+    <h1>Recommended Books</h1>
+    <?php
+    $books = [
+        [
+            'name' => 'Do Androids Dream of Electric Sheep',
+            'author' => 'Philip K. Dick',
+            'purchaseUrl' => 'http://example.com'
+        ],
+        [
+            'name' => 'Project Hail Mary',
+            'author' => 'Andy Weir',
+            'purchaseUrl' => 'http://example.com'
+        ],
+    ]
+
+    ?>
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['author'] ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+
+</html>
