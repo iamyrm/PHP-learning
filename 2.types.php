86fc1356b9b2bed9244e7d111a94844004ed5419
Yagyaraj Majhi <majhiyagya123@gmail.com>
2025-06-03 22:10:36 +0545

    types

---
+++ 2.types.php
@@ -0,0 +1,9 @@
+<?php
+$isStudent = 1;
+var_dump($isStudent, true, $isStudent == true);
+
+$scores = [85, "85", 10.5];
+$total = $scores[0] + $scores[1] + $scores[2];
+var_dump($scores, $total);
+
+echo "The total score is $total \n";
